a8381719a8fdee47066deef75cda71253fc61e8a
snehajgec <sneha.jgec@gmail.com>
2019-05-29 01:23:35 +0530

    created heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, HeroesComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
